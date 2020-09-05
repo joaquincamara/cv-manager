4fc017d37810f03a2e0cac0f819fe347a9ebdd13
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-09-05 17:08:42 -0500

    Updates

---
+++ precache-manifest.8e6f2382057c5123418dc57080bf0341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb838b7ec9dc58ebe7dd0175fce67ece",
+    "url": "/cv-manager/index.html"
+  },
+  {
+    "revision": "8e7999d999c097e7bcd7",
+    "url": "/cv-manager/static/css/main.7903a0a1.chunk.css"
+  },
+  {
+    "revision": "aed3d17cedccca0360ba",
+    "url": "/cv-manager/static/js/2.f97597cb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cv-manager/static/js/2.f97597cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7999d999c097e7bcd7",
+    "url": "/cv-manager/static/js/main.1e3c5187.chunk.js"
+  },
+  {
+    "revision": "0a57edf8fc5488345363",
+    "url": "/cv-manager/static/js/runtime-main.09b81a8b.js"
+  },
+  {
+    "revision": "9d11c3219d7ea004ab84327d27148c5f",
+    "url": "/cv-manager/static/media/alchemist_icon.9d11c321.svg"
+  }
+]);
